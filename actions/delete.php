dab834a65db2d59b84bb0afb66006e04633891b5
asadmukhtarr <axad03213@gmail.com>
2023-09-04 17:35:42 +0500

    Hello Uzm

--- actions/delete.php
+++ actions/delete.php
@@ -1,7 +1,10 @@
 <?php 
     $id = $_GET['id'];
     include('cn.php'); // for connection ..
+    // writing delete query ..
     $query = "DELETE FROM `users` WHERE id='$id'";
+    // for run query ..
     mysqli_query($cn,$query) or die('Cant run query');
+    // redirection ..
     header("Location:../record.php?msg=User Deleted Successfully");
 ?>
